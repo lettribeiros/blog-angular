8dd7f132cf8233cf8f1ef4c8a89a878d417b34fc
lettribeiros <lerihribeiro@gmail.com>
2023-05-06 11:33:24 -0300

    criando novas paginas com content

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "photo":"https://d2d7ho1ae66ldi.cloudfront.net/ArquivoNoticias/fccc6398-0f8f-11ed-aa6e-9587410378a2/tony-stark-snap.jpg",
+        "title":"SAIU NOVO FILME",
+        "description":"Novo filme do homem de ferro conta com super produção e elenco de peso."
+    },
+    {
+        "id":"2",
+        "photo":"https://nosbastidores.com.br/wp-content/uploads/2021/08/Series-Marvel-Disney-Plus-1024x576-1.jpg",
+        "title":"PERSONAGENS QUADJUVANTES",
+        "description":"Personagens quadjuvantes fazem toda a diferença nos filmes da marvel."
+    }
+]
+  
